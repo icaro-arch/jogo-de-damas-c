4f2bc92174f16067ea0b9e5e9007e5a08d80d116
icaro-arch <icr0pc@gmail.com>
2026-06-17 09:57:32 -0300

    Update modo_offline function parameter name
    Renamed parameter in modo_offline function for clarity.

--- modos.c
+++ modos.c
@@ -1,9 +1,21 @@
 #include "damas.h"
 
+//Funcao para o modo de usuario contra usuario
 void modo_usuario_contra_usuario(char tabuleiro[TAM][TAM]) {
+  
+  char jogador_da_vez; // identifica apartir da entrada de texto qual sera o jogador (C ou B)
+  char jogadas[100]; // armazenar jogadas apartir da entrada de texto (formato: “B0--C1” e “I7--H6”)
 
+  printf("Qual jogador fará a primeira jogada? (Digite: C para o de cima e B para o de baixo);
+  scanf("%c", jogador_da_vez"); //entrada para o jogador que vai iniciar a partida
+
+  while(
+
+
+  //loop do jogo
+  
 }
 
-void modo_offline(char tabuleiro[TAM][TAM], const char *nome_arquivo) {
+void modo_offline(char tabuleiro[TAM][TAM], const char *arquivo_com_comandos) {
 
 }
