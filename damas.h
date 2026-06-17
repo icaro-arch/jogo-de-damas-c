75f82bf5f3abae4090bcd42118bd1b37e1126056
icaro-arch <icr0pc@gmail.com>
2026-06-16 21:50:28 -0300

    Add damas.h header file with game prototypes

---
+++ damas.h
@@ -0,0 +1,26 @@
+/* Nomes e Matriculas da equipe:
+PAULO ICARO MATIAS FRANCO - Matricula: 601549
+DAVI OSUGI ALENCAR FERREIRA GOMES - Matricula: 604474
+VICTOR GABRIEL SANTOS MOREIRA - Matricula: 605643
+*/
+
+#ifndef DAMAS_H
+#define DAMAS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// Tamanho do tabuleiro
+#define TAM 10
+
+
+// Protótipos das funções
+// "char tabuleiro[TAM][TAM]" eh a matriz do tabuleiro
+void inicializar_tabuleiro(char tabuleiro[TAM][TAM]);
+void imprimir_tabuleiro(char tabuleiro[TAM][TAM]);
+void modo_usuario_contra_usuario(char tabuleiro[TAM][TAM]);
+void modo_offline(char tabuleiro[TAM][TAM], const char *nome_arquivo);
+
+#endif
