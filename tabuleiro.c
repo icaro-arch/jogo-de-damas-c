0816594431c6518b047776830530ca6c11b45fa4
daviosugi <daviosugi07@gmail.com>
2026-06-17 09:56:17 -0300

    Adicionei a função inicializar correta

--- tabuleiro.c
+++ tabuleiro.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
 
-void testando_git() {
-    printf("OI mundo\n");
+#include "damas.h"
+
+void inicializar_tabuleiro(char tabuleiro[TAM][TAM]){
+
+    for(int linha = 0; linha < TAM; linha++){
+
+        for(int coluna = 0; coluna < TAM; linha++){
+
+            if((linha + coluna) % 2 == 0){
+
+                tabuleiro[linha][coluna] = '#';
+
+            }
+            else {
+
+                if(linha < 3){
+                    tabuleiro[linha][coluna] = 'o';
+                }
+                if(linha > 6){
+                    tabuleiro[linha][coluna] = '@';
+                } 
+                else {
+                    tabuleiro[linha][coluna] = ' ';
+                }
+            }
+        }
+    }
+}
+
+void imprimir_tabuleiro(char tabuleiro[TAM][TAM]){
+    
 }
