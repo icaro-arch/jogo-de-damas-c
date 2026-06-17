54e6735252cdfcb35a0811e3ea91924f489f2801
icaro-arch <icr0pc@gmail.com>
2026-06-17 09:58:31 -0300

    Fix typo in prompt for first player input

--- modos.c
+++ modos.c
@@ -6,7 +6,7 @@ void modo_usuario_contra_usuario(char tabuleiro[TAM][TAM]) {
   char jogador_da_vez; // identifica apartir da entrada de texto qual sera o jogador (C ou B)
   char jogadas[100]; // armazenar jogadas apartir da entrada de texto (formato: “B0--C1” e “I7--H6”)
 
-  printf("Qual jogador fará a primeira jogada? (Digite: C para o de cima e B para o de baixo);
+  printf("Qual jogador fara a primeira jogada? (Digite: C para o de cima e B para o de baixo);
   scanf("%c", jogador_da_vez"); //entrada para o jogador que vai iniciar a partida
 
   while(
